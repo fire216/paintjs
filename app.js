fc648005ddf0f92da071246467709d6a827e84e1
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-07-15 13:27:27 +0900

    2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,27 +10,34 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
                     //offset -> 캔버스 내에 위치값
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting()
-}
-
 if(canvas){
     //마우스를 움직일 때
     canvas.addEventListener("mousemove",onMouseMove);
     //마우스를 클릭했을때
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     //마우스를 클릭을 뗏을 때
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
     //마우스가 캔버스를 벗어났을 때
     canvas.addEventListener("mouseleave", stopPainting);
 }
