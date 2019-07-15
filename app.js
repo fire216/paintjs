af1ba59ff97fe1ca4598071ba5efaa9f652eb8e0
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-07-15 15:59:31 +0900

    2.5 Filling Mode

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = "500";
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -37,6 +41,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -47,10 +52,17 @@ function handleRangeChange(event){
 function handleModeClick(event){
     if(filling === true){
         filling = false;
-        mode.innerText = "Fill";
+        mode.innerText = "FILL";
     }else{
         filling = true;
         mode.innerText = "PAINT";
+       // ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
     }
 }
 
@@ -63,6 +75,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     //마우스가 캔버스를 벗어났을 때
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => 
